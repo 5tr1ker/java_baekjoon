c4b4f5b0f6cbf151db671a4b28c8c3135eb63eea
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-07-07 20:33:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,32 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		int dp[];
+ 
+		dp = new int[input + 1];
 		
-		int data = sc.nextInt();
-		int amount = 0;
-		
-		while(data > 0) {
-			if(data % 5 == 0) {
-				data -= 5;
-				amount++;
-			} else if (data % 3 == 0) {
-				data -= 3;
-				amount++;
-			} else if (data > 5) {
-				data -= 5;
-				amount++;
-			}
-			else {
-				amount = -1;
-				break;
+		for(int i = 1; i <= input && i <= 6; i++) {
+			if(i == 3 || i == 5) {
+				dp[i] = 1;
+			} else {
+				dp[i] = -1;
 			}
 		}
 		
-		System.out.println(amount);
-		sc.close();
+		for(int i = 6; i <= input; i++) {
+			dp[i] = -1;
+			if(dp[i - 5] == -1 && dp[i - 3] == -1) {
+				dp[i] = -1;
+			} else {
+				dp[i] = Math.min(dp[i - 5] != -1 ? dp[i - 5] + 1 : Integer.MAX_VALUE , dp[i - 3] != -1 ? dp[i - 3] + 1 : Integer.MAX_VALUE);				
+			}
+		}
+		
+		System.out.println(dp[input]);
 	}
 
 }
