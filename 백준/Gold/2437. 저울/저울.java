78455595455a907b4c0f496b6b2c458a42315a53
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-08-01 14:00:37 +0900

    [Gold III] Title: 저울, Time: 128 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[N];
+		
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int i;
+		
+		for(i = 0; i < N ; i++) { 
+			arr[i] = Integer.parseInt(st.nextToken()); 
+		}
+		
+		Arrays.sort(arr);
+		
+		int sum = 0;
+		for(i = 0; i < N; i++) {
+			if(arr[i] > sum + 1) {
+				break;
+			}
+			sum += arr[i];
+		}
+		
+		System.out.println(sum + 1);
+	}
+
+}
