84655d918a3e1ef436c12c12c2035ac4b7b8b3b3
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-08-03 20:12:37 +0900

    [Silver I] Title: 행렬, Time: 160 ms, Memory: 15056 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.java
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -1,96 +1,61 @@
-import java.util.Scanner;
-
-/*
- * 왜, 다시 ( 0,0 ) 에서 시작 안해도 될까?
- * 
- * ex. (0,0) 수정 -> ( 0,2 ) 수정 -> ( 0,1 ) 수정 
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] matA;
-	static int[][] matB;
-	static int N , M;
-	static int answer = 0;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		matA = new int [N][M];
-		matB = new int [N][M];
-		
-		
-		// matA input
-		for ( int i=0; i<N; i++) {
-			String str = sc.next();
-			for ( int j=0; j<M; j++) {
-				matA[i][j] = str.charAt(j);
-			}
-		}
-		
-		// matB input
-		for ( int i=0; i<N; i++) {
-			String str = sc.next();
-			for ( int j=0; j<M; j++) {
-				matB[i][j] = str.charAt(j);
-			}
-		}
-		
-		// 뭐 바꿀 수 있는게 없잖아.
-		if ( N < 3 || M < 3) {
-			if ( IsSame(matA,matB) ) {
-				answer = 0;
-			}
-			else {
-				answer = -1;
-			}
-		}
-		// 바꿔보자
-		else {
-			Solution();
-		}
-		
-		if( !IsSame(matA, matB)) {
-			answer = -1;
-		}
-		
-		System.out.println(answer);
-	}
 
-	private static void Solution() {
-		// TODO Auto-generated method stub
-		// 3*3 만 바꿀 수 있으니, ( N-2, M-2 ) 까지밖에 못감
-		for( int i=0; i<N-2 ; i++) {
-			for ( int j=0; j<M-2 ;j ++) {
-				if ( matA[i][j] != matB[i][j]) {
-					filp(i,j);
-					answer ++;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		boolean arr[][] = new boolean[N][M]; // 입력
+		boolean arr2[][] = new boolean[N][M]; // 목표
+		
+		String[] input;
+		int i , j;
+		for(i = 0; i < N ; i++) {
+			input = br.readLine().split("");
+			for(j = 0 ; j < M ; j++) {
+				arr[i][j] = input[j].equals("1") ? true : false;
+			}
+		}
+		
+		for(i = 0; i < N ; i++) {
+			input = br.readLine().split("");
+			for(j = 0 ; j < M ; j++) {
+				arr2[i][j] = input[j].equals("1") ? true : false;
+			}
+		}
+		
+		int count = 0;
+		int k2 , p2;
+		
+		for(i = 0; i < N - 2; i++) {
+			for(j = 0; j < M - 2 ; j++) {
+				if(arr[i][j] != arr2[i][j]) {
+					for(k2 = i ; k2 < i + 3 ; k2++) {
+						for(p2 = j ; p2 < j + 3; p2++) {
+							arr[k2][p2] = !arr[k2][p2];
+						}
+					}
+					count++;
 				}
 			}
 		}
-	}
-
-	private static void filp(int cr, int cn) {
-		// TODO Auto-generated method stub
 		
-		for ( int i=cr; i< cr+3; i++) {
-			for ( int j=cn; j<cn+3; j++) {
-				matA[i][j] = matA[i][j] ^ 1 ; // 뒤집기
+		boolean isChange = true;
+		for(i = 0; i < N && isChange ; i++) {
+			for(j = 0; j < M && isChange ; j++) {
+				if(arr[i][j] != arr2[i][j]) { 
+					isChange = false;
+				}
 			}
 		}
-		
-	}
-
-	private static boolean IsSame(int[][] matA2, int[][] matB2) {
-		// TODO Auto-generated method stub
-		for ( int i=0; i<N; i++) {
-			for ( int j=0; j<M;j ++){
-				if ( matA2[i][j] != matB2[i][j] ) return false; 
-			}
-		}
-		
-		return true;
+		System.out.println(isChange ? count : -1);
 	}
 }
