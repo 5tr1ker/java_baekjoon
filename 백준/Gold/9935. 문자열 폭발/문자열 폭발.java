9809c6fa37a7ff7000521c9fa7dbedcb5a4ed2dd
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-08-11 16:41:10 +0900

    [Gold IV] Title: 문자열 폭발, Time: 372 ms, Memory: 25200 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
 
@@ -9,32 +8,22 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		String str_2 = br.readLine();
-		Stack<Character> stack = new Stack<Character>();
 		
 		StringBuilder sb = new StringBuilder();
 		int str2Length = str_2.length();
 		
 		for(int i = 0; i < str.length(); i++) {
-			stack.add(str.charAt(i));
+			sb.append(str.charAt(i));
 			if(str_2.charAt(str2Length - 1) == str.charAt(i)) {
-				for(int j = 1; j <= str2Length; j++) {
-					if(stack.search(str_2.charAt(str2Length - j)) != j) {
-						break;
-					}
-					if(j == str_2.length()) {
-						for(int z = 0; z < str2Length; z++) {
-							stack.pop();
-						}
-					}
+				for(int j = 1; j <= str2Length && str2Length < str.length(); j++) {
+					if(sb.length() - str2Length < 0) continue;
+					if(sb.charAt(sb.length() - j) != str_2.charAt(str2Length - j)) break;
+					if(j == str2Length) sb.delete(sb.length() - str2Length , sb.length());
 				}
 			}
 		}
 		
-		for(char ch : stack) {
-			sb.append(ch);
-		}
-		
-		if(stack.isEmpty()) System.out.println("FRULA");
+		if(sb.length() == 0) System.out.println("FRULA");
 		else System.out.println(sb);
 	}
 }
