22c0b4e09ab036ba0c99cf3bef318d5b2bf92fa9
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-08-23 14:42:30 +0900

    [Silver I] Title: 나이트의 이동, Time: 308 ms, Memory: 41448 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -7,33 +7,65 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	static class Point{
-		int x , y;
-		Point(int y , int x) {
+		int x , y , cnt;
+		Point(int y , int x , int cnt) {
 			this.y = y;
 			this.x = x;
+			this.cnt = cnt;
 		}
 	}
+	
+	static int coorX[] = {-2 , -2 , 2 , 2 , -1 , 1 , -1 , 1};
+	static int coorY[] = {-1 , 1 , -1 , 1 , -2 , -2 , 2 , 2};
+	static boolean check[][];
+	static int size;
+	
+	public static int bfs(int startY , int startX , int endY , int endX) {
+		ArrayDeque<Point> q = new ArrayDeque<>();
+		
+		q.add(new Point(startY , startX , 0));
+		
+		while(!q.isEmpty()) {
+			Point point = q.poll();
+			int Y = point.y;
+			int X = point.x;
+			int cnt = point.cnt + 1;
+			
+			for(int j = 0; j < 8; j++) {
+				if(X + coorX[j] < 0 || X + coorX[j] >= size || Y + coorY[j] < 0 || Y + coorY[j] >= size) continue;
+				
+				if(endY == Y + coorY[j] && endX == X + coorX[j]) {
+					return cnt;
+				}
+				
+				if(!check[Y + coorY[j]][X + coorX[j]]) {
+					check[Y + coorY[j]][X + coorX[j]] = true;
+					q.add(new Point(Y + coorY[j] , X + coorX[j] , cnt));
+				}
+				
+			}
+		}
+		
+		return 0;
+	}
 
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int arr[][];
+		
 		int testCase = Integer.parseInt(br.readLine());
 		
-		ArrayDeque<Point> q = new ArrayDeque<>();
-		
 		StringTokenizer st;
-		int coorX[] = {-2 , -2 , 2 , 2 , -1 , 1 , -1 , 1};
-		int coorY[] = {-1 , 1 , -1 , 1 , -2 , -2 , 2 , 2};
+		
 		
 		for(int i = 0; i < testCase; i++) {
-			int size = Integer.parseInt(br.readLine());
-			
-			arr = new int[size][size];
+			size = Integer.parseInt(br.readLine());
+
+			check = new boolean[size][size];
 			st = new StringTokenizer(br.readLine(), " ");
 			
 			int start[] = {Integer.parseInt(st.nextToken()) , Integer.parseInt(st.nextToken())};
-			q.add(new Point(start[0] , start[1]));
 			
 			st = new StringTokenizer(br.readLine() , " ");
 			int end[] = {Integer.parseInt(st.nextToken()) , Integer.parseInt(st.nextToken())};
@@ -42,22 +74,7 @@ public class Main {
 				continue;
 			}
 			
-			while(!q.isEmpty()) {
-				Point point = q.poll();
-				int Y = point.y;
-				int X = point.x;
-				
-				for(int j = 0; j < 8; j++) {
-					if(X + coorX[j] < 0 || X + coorX[j] >= size || Y + coorY[j] < 0 || Y + coorY[j] >= size) continue;
-					
-					if(arr[Y + coorY[j]][X + coorX[j]] == 0) {
-						arr[Y + coorY[j]][X + coorX[j]] += arr[Y][X] + 1;
-						q.add(new Point(Y + coorY[j] , X + coorX[j]));
-					}
-				}
-			}
-			
-			System.out.println(arr[end[0]][end[1]]);
+			System.out.println(bfs(start[0] , start[1] , end[0] , end[1]));
 		}
 	}
 }
