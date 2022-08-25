bc8ec5292525ba9e9825c1b47863e0c49daeac5e
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-08-25 15:31:25 +0900

    [Gold IV] Title: 연구소, Time: 492 ms, Memory: 22520 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,18 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static int arr[][];
 	static int virus_arr[][];
+	static boolean check[][];
 	static int N , M;
 	static int coorX[] = {-1,1,0,0};
 	static int coorY[] = {0,0,-1,1};
 	
+	static int virus_count = Integer.MAX_VALUE;
 	static int save_zone = Integer.MIN_VALUE;
 	
 	public static void main(String[] args) throws IOException {
@@ -24,6 +24,7 @@ public class Main {
 		
 		arr = new int[N][M];
 		virus_arr = new int[N][M];
+		check = new boolean[N][M];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine() , " ");
@@ -33,14 +34,14 @@ public class Main {
 		}
 		
 		dfs(0);
-		
+
 		System.out.println(save_zone);
 	}
 
 	public static void dfs(int wall) { // 벽 세우기
 		
 		if(wall == 3) {
-			virus_bfs();
+			virus_dfs();
 			return;
 		}
 		
@@ -55,38 +56,43 @@ public class Main {
 		}
 	}
 	
-	public static void virus_bfs() { // virus_퍼트리기
-		
-		Queue<int []> queue = new LinkedList<int[]>();
-		
+	public static void virus_dfs() { // virus_퍼트리기
+
+		int virus_count2 = 0;
 		for(int i = 0; i < N; i++)
 			for(int j = 0; j < M; j++)
 				virus_arr[i][j] = arr[i][j];
 		
-		for(int i = 0; i < N; i++) 
-			for(int j = 0; j < M; j++) 
-				if(virus_arr[i][j] == 2) 
-					queue.add(new int[] {i , j});
-		
-		
-		while(!queue.isEmpty()) {
-			int index[] = queue.poll();
-			int Y = index[0];
-			int X = index[1];
-			
-			for(int z = 0; z < 4; z++) {
-				int X2 = X + coorX[z];
-				int Y2 = Y + coorY[z];
-				if(X2 < 0 || X2 >= M || Y2 < 0 || Y2 >= N) continue;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				if(virus_arr[i][j] == 2)
+					virus_count2 = virus_diffusion(i, j, virus_count2);					
 				
-				if(virus_arr[Y2][X2] == 0) {
-					virus_arr[Y2][X2] = 2;
-					queue.add(new int[] {Y2 , X2});
-				}
+				if(virus_count2 > virus_count) break;
 			}
+			if(virus_count2 > virus_count) break;
 		}
 		
 		save_bfs();
+		virus_count = Math.min(virus_count2 , virus_count);
+	}
+	
+	public static int virus_diffusion(int Y, int X, int virus_count2) {
+		
+		for(int i = 0; i < 4; i++) {
+			int X2 = X + coorX[i];
+			int Y2 = Y + coorY[i];
+			
+			if(X2 < 0 || X2 >= M || Y2 < 0 || Y2 >= N) continue;
+			
+			if(virus_arr[Y2][X2] == 0) {
+				virus_arr[Y2][X2] = 2;
+				virus_count2 = virus_count2 + 1;
+				virus_count2 = virus_diffusion(Y2, X2, virus_count2);
+			}
+		}
+
+		return virus_count2;
 	}
 	
 	public static void save_bfs() { // 안전지대 찾기
