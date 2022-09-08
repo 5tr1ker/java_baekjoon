050f0e70790eef95f8294352f2048a6a5d3b7902
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-09-08 11:43:55 +0900

    [Silver IV] Title: 큐 2, Time: 1512 ms, Memory: 408040 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> queue = new LinkedList<Integer>();
+		StringBuilder sb = new StringBuilder();
+		
+		int back = -1;
+		for(int i = 0; i < N; i++) {
+			String []str = br.readLine().split(" ");
+			
+			if(str[0].equals("push")) {
+				back = Integer.parseInt(str[1]);
+				queue.add(back);
+			} else if(str[0].equals("pop")) {
+				if(queue.isEmpty()) sb.append("-1\n");
+				else sb.append(queue.poll() + "\n");
+			} else if(str[0].equals("size")) {
+				sb.append(queue.size() + "\n");
+			} else if(str[0].equals("empty")) {
+				if(queue.isEmpty()) sb.append("1\n");
+				else sb.append("0\n");
+			} else if(str[0].equals("front")) {
+				if(queue.isEmpty()) sb.append("-1\n");
+				else sb.append(queue.peek() + "\n");
+			} else if(str[0].equals("back")) {
+				if(queue.isEmpty()) sb.append("-1\n");
+				else sb.append(back + "\n");
+			} 
+		}
+		System.out.println(sb);
+	}
+
+}
