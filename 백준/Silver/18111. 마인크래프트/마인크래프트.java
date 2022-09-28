27145346d938bd0df9df62c284d1fae747d75d0c
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-09-28 16:54:01 +0900

    [Silver II] Title: 마인크래프트, Time: 700 ms, Memory: 34800 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,70 +1,63 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static public int map[][];
+	static public int N , M , block , maxValue = 0 , minValue = 256;
+	static public int result[];
 
-        String[] nmb = br.readLine().split(" ");
-        int n = Integer.parseInt(nmb[0]);
-        int m = Integer.parseInt(nmb[1]);
-        int b = Integer.parseInt(nmb[2]);
-
-        int[][] ground = new int[n][m];
-
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < ground.length; i++) {
-
-            String[] groundRow = br.readLine().split(" ");
-
-            for (int j = 0; j < m; j++) {
-                int value = Integer.parseInt(groundRow[j]);
-                ground[i][j] = value;
-
-                max = Math.max(max, value);
-                min = Math.min(min, value);
-            }
-        }
-
-        int answerSeconds = Integer.MAX_VALUE; // 시간
-        int answerHeight = -1; // 층
-
-        for (int i = min; i <= max; i++) { // 최소층 부터 최대층 까지
-
-            int seconds = 0;
-            int inventory = b;
-
-            for (int j = 0; j < ground.length; j++) {
-                for (int k = 0; k < ground[j].length; k++) {
-                    int diff = ground[j][k] - i;
-
-                    if(diff > 0) { // 제거해야 함
-                        seconds += Math.abs(diff) * 2;
-                        inventory += Math.abs(diff);
-                    }else if(diff < 0){ // 추가해야 함
-                        seconds += Math.abs(diff);
-                        inventory -= Math.abs(diff);
-                    }
-                }
-            }
-
-            if(inventory >= 0) {
-                if(seconds <= answerSeconds) { // == 가 포함되어야 함 ㅇ그렇지 않으면 최대 높이를 판별 하지 못함
-                    answerSeconds = seconds;
-                    answerHeight = i;
-                }
-            }
-        }
-
-        bw.write(answerSeconds + " " + answerHeight);
-
-        bw.flush();
-        bw.close();
-    }
+	public static void bruthForce(int destHeight , int blockCount) {
+		int appendTime = 0;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				if(map[i][j] > destHeight) { // 산을 깎아야한다.
+					int temp = map[i][j] - destHeight;
+					appendTime += (Math.abs(temp) * 2);
+					blockCount += Math.abs(temp);
+				}
+				
+				else if(map[i][j] < destHeight) { // 블록 설치
+					int temp = destHeight - map[i][j];
+					
+					appendTime += Math.abs(temp);
+					blockCount -= Math.abs(temp);
+				}
+			}
+		}
+		
+		if(result[0] >= appendTime && blockCount >= 0) {
+			result[0] = appendTime;
+			result[1] = destHeight;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		block = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		result = new int[2];
+		
+		result[0] = result[1] = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine() , " ");
+			for(int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				minValue = Math.min(minValue, map[i][j]);
+				maxValue = Math.max(maxValue, map[i][j]);
+			}
+		}
+		
+		for(int i = minValue; i <= maxValue; i++) {
+			bruthForce(i , block);
+		}
+		System.out.println(result[0] + " " + result[1]);
+	}
 }
