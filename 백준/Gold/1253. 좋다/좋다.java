ea83b092d282bdf12324998a08180148500389d3
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-11-05 13:02:35 +0900

    [Gold IV] Title: 좋다, Time: 168 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -3,30 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-
-	public static int arr[];
-	public static int N;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine() , " ");
-		arr = new int[N];
+		
+		int arr[] = new int[N];
 		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		Arrays.sort(arr);
 		int count = 0;
-		
 		for(int i = 0; i < N; i++) {
-			int max = N - 1;
 			int min = 0;
+			int max = N - 1;
 			
 			while(max > min) {
-				long sum = arr[min] + arr[max];
+				int sum = arr[min] + arr[max];
 				
 				if(sum == arr[i]) {
 					if(i == min) {
@@ -42,14 +39,12 @@ public class Main {
 						max--;
 					} else {
 						min++;
-					}
+					}					
 				}
-				
 			}
 			
 		}
-		
 		System.out.println(count);
 	}
-
+	
 }
