7d4cfb1c19d70639a4d9f73edd12b8a2f4799093
Park s.j <49367338+5tr1ker@users.noreply.github.com>
2022-11-08 12:35:45 +0900

    [Silver III] Title: 안테나, Time: 584 ms, Memory: 42404 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -1,25 +1,25 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		ArrayList<Integer> al = new ArrayList<Integer>();
-		
-		for(int i=0; i<n; i++) {
-			al.add(sc.nextInt());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+		int arr[] = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Collections.sort(al);
+		Arrays.sort(arr);
 		
-		int median = (n-1) / 2;
+		if(N % 2 == 0) System.out.println(arr[N / 2 - 1]);
+		else System.out.println(arr[N / 2]);
 		
-		System.out.println(al.get(median));
-		
-
 	}
 
 }
