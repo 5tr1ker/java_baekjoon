dfbb2c46028cfcddbcbc51511e99a3b171a00267
Park s.j <follow2272@naver.com>
2022-12-15 17:35:11 +0900

    [Gold III] Title: 앱, Time: 180 ms, Memory: 19800 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -4,35 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine() , " ");
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-	
-		int dp[][] = new int[N][10001];
-		int costArr[] = new int[N];
+		
 		int memoryArr[] = new int[N];
+		int weightArr[] = new int[N];
+		int dp[][] = new int[N][10001];
 		
 		StringTokenizer st1 = new StringTokenizer(br.readLine() , " ");
 		StringTokenizer st2 = new StringTokenizer(br.readLine() , " ");
-		for(int i = 0; i < N; i++) {
+		
+		for(int i = 0; i < N; i++) {			
 			memoryArr[i] = Integer.parseInt(st1.nextToken());
-			costArr[i] = Integer.parseInt(st2.nextToken());
+			weightArr[i] = Integer.parseInt(st2.nextToken());
 		}
 		
 		int answer = Integer.MAX_VALUE;
 		for(int i = 0; i < N; i++) {
 			int memory = memoryArr[i];
-			int cost = costArr[i];
+			int weight = weightArr[i];
 			
 			for(int j = 0; j <= 10000; j++) {
 				if(i == 0) {
-					if(j >= cost) dp[i][j] = memory;
+					if(j >= weight) dp[i][j] = memory;
 				} else {
-					if(j >= cost) dp[i][j] = Math.max(dp[i - 1][j - cost] + memory , dp[i - 1][j]);
+					if(j >= weight) dp[i][j] = Math.max(dp[i - 1][j - weight] + memory , dp[i - 1][j]);
 					else dp[i][j] = dp[i - 1][j];
 				}
 				
@@ -42,5 +42,4 @@ public class Main {
 		
 		System.out.println(answer);
 	}
-
 }
