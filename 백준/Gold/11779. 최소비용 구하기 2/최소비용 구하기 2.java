9dcfd33c2cde4e7dc67ee98b1b6073230fb2cf13
Park s.j <follow2272@naver.com>
2022-12-22 17:40:36 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 544 ms, Memory: 51292 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -26,11 +26,12 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
 		ArrayList<ArrayList<Point>> list = new ArrayList<ArrayList<Point>>();
-		StringTokenizer st;
-		for(int i = 0; i <= N; i++)
+		for(int i = 0; i <= N; i++) {
 			list.add(new ArrayList<Point>());
+		}
 		
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine() , " ");
@@ -44,50 +45,48 @@ public class Main {
 		st = new StringTokenizer(br.readLine() , " ");
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
-		
-		int weight[] = new int[N + 1];
-		int prev[] = new int[N + 1];
-		
-		for(int i = 0; i <= N; i++)
-			weight[i] = INF;
-		
 		PriorityQueue<Point> queue = new PriorityQueue<Point>(new Comparator<Point>() {
+
 			@Override
 			public int compare(Point o1, Point o2) {
 				return o1.weight - o2.weight;
 			}
 		});
 		
-		// 다익스트라
-		queue.offer(new Point(start , 0));
+		int weight[] = new int[N + 1];
+		int prev[] = new int[N + 1];
+		for(int i = 1; i <= N; i++)
+			weight[i] = INF;
+		
 		weight[start] = 0;
+		queue.offer(new Point(start , 0));
 		while(!queue.isEmpty()) {
 			Point point = queue.poll();
 			
 			if(point.weight > weight[point.vertex]) continue;
-			
 			for(Point data : list.get(point.vertex)) {
-				if(weight[data.vertex] > data.weight + weight[point.vertex]) {
-					weight[data.vertex] = data.weight + weight[point.vertex];
+				if(weight[data.vertex] > weight[point.vertex] + data.weight) {
+					weight[data.vertex] = weight[point.vertex] + data.weight;
 					prev[data.vertex] = point.vertex;
 					queue.offer(new Point(data.vertex , weight[data.vertex]));
 				}
 			}
 		}
 		
+		StringBuilder sb = new StringBuilder();
 		Stack<Integer> stack = new Stack<Integer>();
 		stack.push(end);
 		int idx = end;
-		while(prev[idx] != 0) {
+		while(weight[idx] != 0) {
 			stack.push(prev[idx]);
 			idx = prev[idx];
 		}
-		StringBuilder sb = new StringBuilder();
-		sb.append(weight[end]).append("\n").append(stack.size()).append("\n");
 		
+		sb.append(weight[end]).append("\n").append(stack.size()).append("\n");
 		while(!stack.isEmpty()) {
 			sb.append(stack.pop()).append(" ");
 		}
+		
 		System.out.println(sb);
 	}
 }
