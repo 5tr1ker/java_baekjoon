960a83e07118a090b5bffaaf117d3022f1cdeb2a
Park s.j <follow2272@naver.com>
2023-01-05 15:58:39 +0900

    [Gold IV] Title: 네트워크 연결, Time: 584 ms, Memory: 48896 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Point {
+	int start;
+	int end;
+	int weight;
+	
+	public Point(int start , int end , int weight) {
+		this.start = start;
+		this.end = end;
+		this.weight = weight;
+	}
+}
+
+public class Main {
+	static int parent[];
+	
+	static public int findUnion(int idx) {
+		if(parent[idx] == idx) return idx;
+		
+		return parent[idx] = findUnion(parent[idx]);
+	}
+	
+	static public void union(int idx1 , int idx2) {
+		int n1 = findUnion(idx1);
+		int n2 = findUnion(idx2);
+		
+		parent[n1] = n2;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		parent = new int[N + 1];
+		Point point[] = new Point[M];
+		
+		for(int i = 1; i <= N ; i++)
+			parent[i] = i;
+		
+		StringTokenizer st;
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine() , " ");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			
+			point[i] = new Point(start , end , weight);
+		}
+		
+		Arrays.sort(point , new Comparator<Point>() {
+			@Override
+			public int compare(Point o1, Point o2) {
+				// TODO Auto-generated method stub
+				return o1.weight - o2.weight;
+			}
+		});
+		
+		int weight = 0;
+		for(int i = 0; i < M; i++) {
+			Point pnt = point[i];
+			
+			int startIdx = findUnion(pnt.start);
+			int endIdx = findUnion(pnt.end);
+			
+			if(startIdx != endIdx) {
+				weight += pnt.weight;
+				union(startIdx, endIdx);
+			}
+		}
+		System.out.println(weight);
+	}
+	
+}
