809cfed303abea70a6b5bf10ee9c46ecc020d1c0
Park s.j <follow2272@naver.com>
2023-02-17 01:06:40 +0900

    [Gold IV] Title: 팰린드롬?, Time: 856 ms, Memory: 219152 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -9,37 +9,35 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int arr[] = new int[N + 1];
-		int dp[][] = new int[N + 1][N + 1];
+		boolean dp[][] = new boolean[N + 1][N + 1];
 		StringTokenizer st = new StringTokenizer(br.readLine() , " ");
 		for(int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			dp[i][i] = 1;
+			dp[i][i] = true;
 		}
-		int M = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
 		
-		// 범위 2
+		// 2번째 항
 		for(int i = 1; i < N; i++) {
-			if(arr[i] == arr[i + 1]) {
-				dp[i][i + 1] = 1;
-			}
+			if(arr[i] == arr[i + 1])
+				dp[i][i + 1] = true;
 		}
 		
-		// 범위 3 ~
-		for(int i = 2; i < N; i++) {
-			for(int j = 1; j <= N - i; j++) {
-				if(arr[j] == arr[i + j] && dp[j + 1][i + j - 1] == 1) {
-					dp[j][j + i] = 1;
+		// 3번째 항
+		for(int i = 2; i < N ; i++) {
+			for(int j = 1; j <= N - i ; j++) {
+				if(arr[j] == arr[i + j] && dp[j + 1][i + j - 1]) {
+					dp[j][i + j] = true;
 				}
 			}
 		}
 		
+		int M = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine() , " ");
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			
-			sb.append(dp[start][end]).append("\n");
+			sb.append(dp[start][end] ? 1 : 0).append("\n");
 		}
 		
 		System.out.println(sb);
