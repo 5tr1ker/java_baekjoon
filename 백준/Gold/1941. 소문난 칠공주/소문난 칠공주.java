1e7052b6b9ee50c09f2e7181539e7e4cdbac760a
Park s.j <follow2272@naver.com>
2023-02-18 19:55:19 +0900

    [Gold III] Title: 소문난 칠공주, Time: 444 ms, Memory: 69660 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -6,43 +6,41 @@ import java.util.Queue;
 
 public class Main {
 	
+	static int select[];
 	static char arr[][];
 	static int ans = 0;
 	static int coorX[] = {-1,1,0,0};
 	static int coorY[] = {0,0,-1,1};
-	
-	// 조합론
 	static int comX[];
 	static int comY[];
-	static int select[];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		arr = new char[5][5];
+		select = new int[7];
 		comX = new int[25];
 		comY = new int[25];
-		select = new int[7];
-		
-		for(int i = 0 ; i < 5; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < 5; j++) {
-				arr[i][j] = str.charAt(j);
-			}
-		}
 		
 		for(int i = 0; i < 25; i++) {
 			comX[i] = i / 5;
 			comY[i] = i % 5;
 		}
 		
-		combinate(0, 0, 7);
+		for(int i = 0; i < 5; i++) {
+			String str = br.readLine();
+			for(int j = 0; j < 5; j++) {
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		
+		combination(0,0,7);
 		
 		System.out.println(ans);
 		
 	}
 	
-	public static void combinate(int idx , int cnt , int left) {
-		if(left <= 0) {
+	public static void combination(int idx , int cnt , int left) { // idx 선택 인덱스 , cnt 인덱스 번호 , left 남은 갯수
+		if(left == 0) {
 			bfs();
 			return;
 		}
@@ -50,40 +48,42 @@ public class Main {
 		if(cnt >= 25) return;
 		
 		select[idx] = cnt;
-		combinate(idx + 1 , cnt + 1 , left - 1); // 선택하기
-		combinate(idx , cnt + 1 , left); // 버리기
+		combination(idx + 1 , cnt + 1 , left - 1); // 선택합니다.
+		combination(idx , cnt + 1, left); // 선택하지 않습니다.
 	}
 	
-	// 시작
-	public static void bfs() {
-		Queue<Integer> q = new LinkedList<>();
-        boolean visited[] = new boolean[7];
-        
-        visited[0] = true;
-        q.add(select[0]);
-        int cnt = 1, sCnt = 0;
-        
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            if(arr[comY[cur]][comX[cur]] == 'S') sCnt++;
-            
-            for(int i=0; i<4; i++) {
-                for(int next=1; next<7; next++) {
-                    if(!visited[next] && comX[cur]+coorX[i] == comX[select[next]] && comY[cur]+coorY[i] == comY[select[next]]) {
-                        visited[next] = true;
-                        q.add(select[next]);
-                        cnt++;
-                    }
-                }
-            }
-        }
-        
-        /* 7개 모두 연결되어 있다면 */
-        if(cnt == 7) {
-            if(sCnt >=4) {
-                ans++;
-            }
-        }
+	public static void bfs() { // 연결 && 가능한 조합인가?
+		Queue<Integer> queue = new LinkedList<Integer>();
+		boolean check[] = new boolean[7];
+		check[0] = true;
+		queue.offer(select[0]);
+		
+		int countQueue = 1;
+		int countS = 0;
+		
+		while(!queue.isEmpty()) {
+			int data = queue.poll();
+			
+			if(arr[comX[data]][comY[data]] == 'S') {
+				countS++;
+			}
+			
+			for(int i = 0; i < 4; i++) {
+				int dx = comX[data] + coorX[i];
+				int dy = comY[data] + coorY[i];
+				for(int j = 1; j < 7; j++) {
+					if(!check[j] && comX[select[j]] == dx && comY[select[j]] == dy) {
+						check[j] = true;
+						queue.offer(select[j]);
+						countQueue++;
+					}
+				}
+			}
+		}
+		
+		if(countQueue >= 7 && countS >= 4) {
+			ans++;
+		}
 	}
 	
 }
