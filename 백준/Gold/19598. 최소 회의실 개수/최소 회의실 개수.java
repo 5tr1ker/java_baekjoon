d20faab4edc82b8afb9c434ad3e64350da6544e8
Park s.j <follow2272@naver.com>
2023-02-26 12:02:25 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 584 ms, Memory: 47880 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Meeting {
+	int time;
+	boolean isStart;
+
+	public Meeting(int time, boolean isStart) {
+		this.time = time;
+		this.isStart = isStart;
+	}
+
+	@Override
+	public String toString() {
+		return "Meeting [time=" + time + ", isStart=" + isStart + "]";
+	}
+	
+	
+}
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		PriorityQueue<Meeting> queue = new PriorityQueue<Meeting>(new Comparator<Meeting>() {
+
+			@Override
+			public int compare(Meeting o1, Meeting o2) {
+				return o1.time - o2.time;
+			}
+			
+		});
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine() , " ");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			queue.add(new Meeting(start , true));
+			queue.add(new Meeting(end , false));
+		}
+		
+		
+		int cnt = 0;
+		int ans = 0;
+		while(!queue.isEmpty()) {
+			Meeting data = queue.poll();
+			
+			if(data.isStart) {
+				cnt++;
+				ans = Math.max(ans, cnt);
+			} else {
+				cnt--;
+			}
+		}
+		
+		System.out.println(ans);
+		
+	}
+	
+}
