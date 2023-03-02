ecc65a38289a54be77131f64a53ede6ee2b8e931
Park s.j <follow2272@naver.com>
2023-03-02 12:07:01 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N , M , L;
+	static int arr[];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		arr = new int[N + 2]; // 시작지점 + 끝 지점
+		
+		int min = 1 , max = L;
+		st = new StringTokenizer(br.readLine() , " ");
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		arr[N + 1] = L;
+		Arrays.sort(arr);
+		
+		while(min < max) {
+			int half = (min + max) / 2;
+			
+			if(solved(half) > M) {
+				min = half + 1;
+			} else {
+				max = half;
+			}
+		}
+		
+		System.out.println(min);
+	}
+	
+	public static int solved(int idx) {
+		int count = 0;
+		
+		for(int i = 0; i < N + 1; i++) {
+			count += (arr[i + 1] - arr[i] - 1) / idx;
+		}
+		
+		return count;
+	}
+	
+}
