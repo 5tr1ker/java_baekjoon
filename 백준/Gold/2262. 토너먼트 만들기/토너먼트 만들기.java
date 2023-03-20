44cfc8e0023c55f6ab187e9cacdcb949e716b4f8
Park s.j <follow2272@naver.com>
2023-03-20 12:20:34 +0900

    [Gold IV] Title: 토너먼트 만들기, Time: 152 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Gold/2262. 토너먼트 만들기/토너먼트 만들기.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static public LinkedList<Integer> arr;
+	static public int N, ans;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		arr = new LinkedList<Integer>();
+		for (int i = 0; i < N; i++) {
+			arr.add(Integer.parseInt(st.nextToken()));
+		}
+
+		ans = 0;
+		while (arr.size() > 1) {
+			int index = find();
+			
+			if(index + 1 >= arr.size()) { // 가장 오른쪽에 있는 수
+				ans += Math.abs(arr.get(index) - arr.get(index - 1));
+				arr.remove(index);
+				continue;
+			}
+			
+			if(index - 1 < 0) { // 가장 왼쪽에 있는 수
+				ans += Math.abs(arr.get(index) - arr.get(index + 1));
+				arr.remove(index);
+				continue;
+			}
+			
+			int left = Math.abs(arr.get(index) - arr.get(index - 1));
+			int right = Math.abs(arr.get(index) - arr.get(index + 1));
+			
+			if(left < right) {
+				ans += left;
+			} else {
+				ans += right;
+			}
+			arr.remove(index);
+		}
+
+		System.out.println(ans);
+
+	}
+
+	public static int find() {
+		int max = 0;
+		int index = 0;
+		
+		for(int i = 0; i < arr.size(); i++) {
+			if(max < arr.get(i)) {
+				max = arr.get(i);
+				index = i;
+			}
+		}
+		
+		return index;
+	}
+
+}
