f8933cf494a2d798ec8dd3a5797d5ccbd5884978
Park s.j <follow2272@naver.com>
2023-05-08 16:45:53 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 392 ms, Memory: 21928 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 1; j < N - i; j++) {
+                System.out.printf(" ");
+            }
+            for(int j = 0; j <= i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
