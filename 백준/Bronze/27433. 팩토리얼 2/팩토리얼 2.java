c2a085111bcb2382746b6f795d487e76015ff5e9
Park s.j <follow2272@naver.com>
2023-07-12 22:59:18 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+
+        long ans = 1;
+        for(int i = 2; i <= input; i++) {
+            ans *= i;
+        }
+
+        System.out.println(ans);
+    }
+}
