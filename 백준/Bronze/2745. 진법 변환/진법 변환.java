23d2130527ec0efa895ff61332e3ae21cba336d1
Park s.j <follow2272@naver.com>
2023-10-10 13:51:11 +0900

    [Bronze II] Title: 진법 변환, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.parseInt(N , B));
+
+    }
+}
