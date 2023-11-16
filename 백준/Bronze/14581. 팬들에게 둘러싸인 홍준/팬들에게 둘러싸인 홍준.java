7bab7c06879978f125b9033f75ecbe2244235167
Park s.j <follow2272@naver.com>
2023-11-16 18:53:25 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 152 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String id = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+ id + "::fan:");
+        System.out.println(":fan::fan::fan:");
+
+    }
+}
