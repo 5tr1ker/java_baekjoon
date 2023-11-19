21700e046bfae4efd8701080d9ed76aea1312b5e
Park s.j <follow2272@naver.com>
2023-11-19 22:14:52 +0900

    [Bronze V] Title: 연세대학교, Time: 128 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int input = Integer.parseInt(br.readLine());
+
+        if(input == 0) {
+            System.out.println("YONSEI");
+
+            return;
+        }
+
+        System.out.println("Leading the Way to the Future");
+
+    }
+}
