473fc78e66a1a5ec340c9727e062dd943db9dc37
Park s.j <follow2272@naver.com>
2023-11-27 16:26:14 +0900

    [Silver I] Title: 카잉 달력, Time: 316 ms, Memory: 18124 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while(T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+
+            int ans = -1;
+            for(int i = x; i <= M * N; i += M) {
+                if(i % N == y) {
+                    ans = i + 1;
+                    break;
+                }
+            }
+
+            sb.append(ans).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
