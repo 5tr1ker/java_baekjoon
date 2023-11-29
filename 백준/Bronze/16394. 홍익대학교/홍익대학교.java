c0238ca5d80ccdfcaf163a6c18c0b00d225496ff
Park s.j <follow2272@naver.com>
2023-11-29 23:39:13 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String args[]) throws IOException {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        System.out.println(a - 1946);
+    }
+}
