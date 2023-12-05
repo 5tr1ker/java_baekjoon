569a8f0e08225852e5c32792a09a28c0e62e84fc
Park s.j <follow2272@naver.com>
2023-12-05 17:42:03 +0900

    [Gold IV] Title: 사냥꾼, Time: 648 ms, Memory: 51116 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.java
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.java
@@ -8,7 +8,6 @@ public class Main {
 
 	static public int N , M , L;
 	static public int sade[];
-	static public int arr[][];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,40 +17,53 @@ public class Main {
 		L = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine() , " ");
 		sade = new int[N];
-		arr = new int[M][2];
 		
 		for(int i = 0; i < N; i++) {
 			sade[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine() , " ");
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
 		Arrays.sort(sade);
 		
-		int min , max;
 		int count = 0;
 		for(int i = 0; i < M; i++) {
-			min = 0; max = N - 1;
-			while(min < max) { // x에 가까운 사대 찾기
-				int half = (min + max) / 2;
-				
-				if(sade[half] < arr[i][0]) {	// 좌표 x 보다 낮다면?
-					min = half + 1;
-				} else { 	// 사대의 좌표가 더 높다면?
-					max = half;
-				}
-			}
-			// System.out.printf("half : %d , min : %d , max : %d 새 : %d \n" , (min+max)/2 , min , max , i);
-			if(Math.abs(sade[min] - arr[i][0]) + arr[i][1] <= L) {
+			st = new StringTokenizer(br.readLine() , " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			int result = binarySearch(x , y , 0 , N - 1);
+			
+			if(Math.abs(sade[result] - x) + y <= L) {
 				count++;
-				// System.out.printf("새 x : %d , y : %d 를 잡은 사대 사대 %d \n" , arr[i][0] , arr[i][1] , sade[min]);
 			}
 		}
 		System.out.println(count);
+	}
+	
+	static public int binarySearch(int x , int y , int N , int M) {
+		int min = N;
+		int max = M;
+		
+		while(min < max) { // x에 가까운 사대 찾기
+			int half = (min + max) / 2;
+			
+			if (Math.abs(sade[half] - x) + y <= L || sade[half] == x)
+				return half;
+			
+			if(sade[half] < x) {	// 좌표 x 보다 낮다면?
+				min = half + 1;
+			} else { 	// 사대의 좌표가 더 높다면?
+				max = half;
+			}
+		}
+		
+		if(min > M) {
+			return max;
+		}
+		
+		if(M < N) {
+			return min;
+		}
+		return (Math.abs(sade[min] - x) < Math.abs(sade[max] - x) ? min : max);
 	}
 
 }
