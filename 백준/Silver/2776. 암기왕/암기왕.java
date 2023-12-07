40f53f2b76f0b2a13968529d2318f28330ca9e81
Park s.j <follow2272@naver.com>
2023-12-07 13:27:19 +0900

    [Silver IV] Title: 암기왕, Time: 1740 ms, Memory: 289604 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -3,8 +3,6 @@ import java.io.*;
 
 class Main {
 
-    public static int note_1[] , N;
-
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
@@ -13,14 +11,13 @@ class Main {
         while(T-- > 0) {
             StringBuilder sb = new StringBuilder();
 
-            N = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
 
-            note_1 = new int[N];
+            HashSet<Integer> note_1 = new HashSet<>();
             for(int i = 0; i < N; i++) {
-                note_1[i] = Integer.parseInt(st.nextToken());
+                note_1.add(Integer.parseInt(st.nextToken()));
             }
-            Arrays.sort(note_1);
 
             int M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
@@ -28,7 +25,7 @@ class Main {
             for(int i = 0; i < M; i++) {
                 int next = Integer.parseInt(st.nextToken());
 
-                if(solved(next)) {
+                if(note_1.contains(next)) {
                     sb.append(1).append("\n");
                 }else{
                     sb.append(0).append("\n");
@@ -38,23 +35,6 @@ class Main {
 
             System.out.println(sb);
         }
-    }
-
-    public static boolean solved(int index) {
-        int start = 0;
-        int end = N - 1;
-
-        while(start < end) {
-            int half = (start + end) / 2;
-
-            if(note_1[half] >= index) {
-                end = half;
-            } else {
-                start = half + 1;
-            }
-        }
-
-        return index == note_1[end];
     }
 
 }
