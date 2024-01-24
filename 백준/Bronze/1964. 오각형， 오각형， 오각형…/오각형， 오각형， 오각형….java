ed441eaa085337e1dee06f0a7e6f68509ed3c913
Sangjin Park <follow2272@naver.com>
2024-01-24 12:41:13 +0900

    [Bronze II] Title: 오각형, 오각형, 오각형…, Time: 132 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/1964. 오각형， 오각형， 오각형…/오각형， 오각형， 오각형….java
+++ 백준/Bronze/1964. 오각형， 오각형， 오각형…/오각형， 오각형， 오각형….java
@@ -7,13 +7,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        BigInteger big = new BigInteger("1");
-        int sum = 4;
+        long dot = 1;
+        long sum = 4;
         for(int i = 1; i <= N; i++ , sum += 3) {
-            big = big.add(new BigInteger(String.valueOf(sum)));
+            dot += sum;
         }
 
-        System.out.println(big.mod(new BigInteger(String.valueOf(45678))));
+        System.out.println(dot % 45678);
         // 1 5 ( + 4 ) , 2 12 ( + 7 ) , 3 22 ( + 10 ) , 4 35 ( + 13 )
         //
     }
