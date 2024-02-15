dc1322517f017254eb2b7d9c43ec6426ae3f91c1
Sangjin Park <follow2272@naver.com>
2024-02-15 23:40:54 +0900

    [Bronze IV] Title: 과자, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze IV/10156. 과자/과자.java
@@ -0,0 +1,26 @@
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if(K * N > M) {
+            System.out.println(K * N - M);
+
+            return;
+        }
+
+        System.out.println(0);
+
+    }
+
+}
