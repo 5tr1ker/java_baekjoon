21c27bf38fd4d25ecffa0c754f65f00dd908c945
Sangjin Park <follow2272@naver.com>
2024-05-09 16:30:01 +0900

    [Bronze IV] Title: 한글 2, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,9 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println((int)scan.next().charAt(0)-44031);
+    }
+}
