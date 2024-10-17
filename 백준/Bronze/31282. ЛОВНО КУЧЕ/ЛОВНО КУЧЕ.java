81542529d35084d3a7a53a6af51f7d9d928ea3d4
Sangjin Park <follow2272@naver.com>
2024-10-17 23:34:15 +0900

    [Bronze IV] Title: ЛОВНО КУЧЕ, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/31282. ЛОВНО КУЧЕ/ЛОВНО КУЧЕ.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        while(N > 0) {
+            ans++;
+            N -= ( K - M );
+        }
+
+        System.out.println(ans);
+    }
+}
