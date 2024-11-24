3afc89ef638f91e6a2bbbb52c58cd8fe07201364
Sangjin Park <follow2272@naver.com>
2024-11-24 20:47:30 +0900

    [Bronze III] Title: 도미노, Time: 108 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2921. 도미노/도미노.java
+++ 백준/Bronze/2921. 도미노/도미노.java
@@ -1,22 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int sum = 0;
-		
-		//0일때는 0밖에 없으니 1부터 시작한다. N까지.
-		for(int i = 1; i <= N; i++) {
-			//현재 값의 2배까지 증가하는 값
-			for(int j = i; j <= i * 2; j++) {
-				sum += j;
-			}
-		}
-		System.out.println(sum);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = 0; j <= i; j++) {
+                ans += i + j;
+            }
+        }
+
+        System.out.println(ans);
+        // 4 = 1 2 3 4 5 6 7 8
+        // 3 = 1 2 3 4 5 6 7 8 9
+        // 2 = 1 2 3 4
+        // 1 = 1 2
+    }
 }
